ad611021addf637717016551cdab8727e7897c3e
emirhansimsekk <20120205046@ismu.edu.tr>
2023-03-14 13:52:17 +0300

    ebsNakliyat

---
+++ app/src/main/java/com/emirhansimsek/ebsnakliyat/Siparislerim.java
@@ -0,0 +1,14 @@
+package com.emirhansimsek.ebsnakliyat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Siparislerim extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_siparislerim);
+    }
+}
